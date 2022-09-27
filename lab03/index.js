2b42b5475e5925d95d5224589df08bf2fcf34298
Titanhood <ajay.srivastav0070@gmail.com>
2022-09-27 18:49:03 -0400

    added lab03

---
+++ lab03/index.js
@@ -0,0 +1,101 @@
+
+var plugin = function (options) {
+    var seneca = this;
+
+    seneca.add({ role: 'employee', cmd: 'add' }, function (msg, respond) {
+        this.make('employee').data$(msg.data).save$(respond);
+    });
+
+    seneca.add({ role: 'employee', cmd: 'get' }, function (msg, respond) {
+        this.make('employee').load$(msg.data.user_id, respond);
+    });
+
+    seneca.add({ role: 'employee', cmd: 'get-all' }, function (msg, respond) {
+        this.make('employee').list$({}, respond);
+    });
+
+    seneca.add({ role: 'employee', cmd: 'delete' }, function (msg, respond) {
+        this.make('employee').remove$(msg.data.user_id, respond);
+    });
+
+
+}
+
+module.exports = plugin;
+
+
+
+var seneca = require("seneca")();
+seneca.use(plugin);
+seneca.use('seneca-entity');
+
+seneca.add('role:api, cmd:add-user', function (args, done) {
+    console.log("--> cmd:add-user");
+    var employee = {
+        firstname: args.firstname,
+        lastname: args.lastname,
+        position: args.position
+    }
+    console.log("--> employee: " + JSON.stringify(employee));
+    seneca.act({ role: 'employee', cmd: 'add', data: employee }, function (err, msg) {
+        console.log(msg);
+        done(err, msg);
+    });
+});
+
+seneca.add('role:api, cmd:get-all-users', function (args, done) {
+    console.log("--> cmd:get-all-users");
+    seneca.act({ role: 'employee', cmd: 'get-all' }, function (err, msg) {
+        console.log(msg);
+        done(err, msg);
+    });
+});
+
+seneca.add('role:api, cmd:get-user', function (args, done) {
+    console.log("--> cmd:get-user, args.user_id: " + args.user_id);
+    seneca.act({ role: 'employee', cmd: 'get', data: { user_id: args.user_id } }, function (err, msg) {
+        console.log(msg);
+        done(err, msg);
+    });
+});
+
+
+seneca.add('role:api, cmd:delete-user', function (args, done) {
+    console.log("--> cmd:delete-user, args.user_id: " + args.user_id);
+    seneca.act({ role: 'employee', cmd: 'delete', data: { user_id: args.user_id } }, function (err, msg) {
+        console.log(msg);
+        done(err, msg);
+    });
+});
+
+seneca.add('role:api, cmd:delete-all-users', function (args, done) {
+    done(null, { cmd: "delete-all-users" });
+});
+
+seneca.act('role:web', {
+    use: {
+        prefix: '/abservice',
+        pin: { role: 'api', cmd: '*' },
+        map: {
+            'add-user': { GET: true },
+            'get-all-users': { GET: true },
+            'get-user': { GET: true, },
+            'delete-user': { GET: true, }
+        }
+    }
+})
+
+var express = require('express');
+var app = express();
+app.use(require("body-parser").json())
+app.use(seneca.export('web'));
+
+
+
+app.listen(3000)
+console.log("Server listening on localhost:3000 ...");
+console.log("----- Requests -------------------------");
+console.log("http://localhost:3000/abservice/add-user?firstname=Peter&lastname=Doe&position=Developer");
+console.log("http://localhost:3000/abservice/get-all-users");
+console.log("http://localhost:3000/abservice/get-user?user_id=1245");
+console.log("http://localhost:3000/abservice/delete-user?user_id=1245");
